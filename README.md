3612f91973fca1a076f4a08f59afbe29e944b57e
Chad Walker <chad@chad-cat-lore-eddie.com>
2016-06-20 16:51:15 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # safe_async
+
+This is a wrapper around [async](caolan/async) functions so that the
+callback won't be called until all started tasks have called their
+callbacks. Standard *async* usually calls the callback right away on
+an error, but that can mean that there are orphaned tasks that don't
+know an error happened, so they are still trying to do their
+work. Usually this is fine, but if in the callback you have to remove
+some resources that the other task expects to be there, then you can
+get into a bad position. *safe_async* wraps the tasks and callback
+functions to monitor their progress, and will only call the final
+callback when all the tasks have completed, even if there was an error.
---
+++ index.js
@@ -0,0 +1,127 @@
+'use strict';
+
+var async = require('async');
+
+var Finalizer = function Finalizer() {
+    this.calls = 0;
+    this.final_callback = null;
+    this.final_callback_args = null;
+};
+
+Finalizer.prototype.wrap = function (func) {
+    return (...args) => {
+        this.calls += 1;
+        var callback = args.pop();
+        args.push((...cb_args) => {
+            this.calls -= 1;
+
+            callback.apply(null, cb_args);
+            if (this.calls === 0 && this.final_callback) {
+                return this.final_callback.apply(null, this.final_callback_args);
+            }
+        });
+
+        return func.apply(null, args);
+    };
+};
+
+Finalizer.prototype.wrap_auto = function (func) {
+    return (...args) => {
+        this.calls += 1;
+        var callback = args.shift();
+        args.unshift((...cb_args) => {
+            this.calls -= 1;
+
+            callback.apply(null, cb_args);
+            if (this.calls === 0 && this.final_callback) {
+                return this.final_callback.apply(null, this.final_callback_args);
+            }
+        });
+
+        return func.apply(null, args);
+    };
+};
+
+Finalizer.prototype.callback = function (callback) {
+    var cb_func = (...args) => {
+        if (this.calls > 0) {
+            if (!this.final_callback) {
+                this.final_callback = callback;
+                this.final_callback_args = args;
+            }
+            return;
+        }
+        callback.apply(null, args);
+    };
+    return cb_func;
+};
+
+var wrapped_functions = {
+    each: (arr, iterator, cb) => {
+        var f = new Finalizer();
+        return async.each(arr, f.wrap(iterator), f.callback(cb));
+    },
+    eachLimit: (arr, limit, iterator, cb) => {
+        var f = new Finalizer();
+        return async.eachLimit(arr, limit, f.wrap(iterator), f.callback(cb));
+    },
+    map: (arr, iterator, cb) => {
+        var f = new Finalizer();
+        return async.map(arr, f.wrap(iterator), f.callback(cb));
+    },
+    mapLimit: (arr, limit, iterator, cb) => {
+        var f = new Finalizer();
+        return async.mapLimit(arr, limit, f.wrap(iterator), f.callback(cb));
+    },
+    parallel: (arr_or_obj, cb) => {
+        var f = new Finalizer();
+        if (Array.isArray(arr_or_obj)) {
+            arr_or_obj = arr_or_obj.map(func => f.wrap(func));
+        } else {
+            arr_or_obj = Object.keys(arr_or_obj).reduce((obj, func_name) => {
+                obj[func_name] = f.wrap(arr_or_obj[func_name]);
+                return obj;
+            }, {});
+        }
+        return async.parallel(arr_or_obj, f.callback(cb));
+    },
+    parallelLimit: (arr_or_obj, limit, cb) => {
+        var f = new Finalizer();
+        if (Array.isArray(arr_or_obj)) {
+            arr_or_obj = arr_or_obj.map(func => f.wrap(func));
+        } else {
+            arr_or_obj = Object.keys(arr_or_obj).reduce((obj, func_name) => {
+                obj[func_name] = f.wrap(arr_or_obj[func_name]);
+                return obj;
+            }, {});
+        }
+        return async.parallelLimit(arr_or_obj, limit, f.callback(cb));
+    },
+    times: (n, iterator, cb) => {
+        var f = new Finalizer();
+        return async.times(n, f.wrap(iterator), f.callback(cb));
+    },
+    timesLimit: (n, limit, iterator, cb) => {
+        var f = new Finalizer();
+        return async.timesLimit(n, limit, f.wrap(iterator), f.callback(cb));
+    },
+    auto: (tasks, concurrency, cb) => {
+        var f = new Finalizer();
+        Object.keys(tasks).forEach(task_name => {
+            var arr_or_func = tasks[task_name];
+            if (Array.isArray(arr_or_func)) {
+                arr_or_func.push(f.wrap_auto(arr_or_func.pop()));
+            } else {
+                arr_or_func = f.wrap_auto(arr_or_func);
+            }
+            tasks[task_name] = arr_or_func;
+        });
+        return async.auto(tasks, concurrency, f.callback(cb));
+    }
+};
+
+var exports = {};
+Object.keys(async).forEach(name => exports[name] = async[name]);
+Object.keys(wrapped_functions).forEach(name => exports[name] = wrapped_functions[name]);
+
+module.exports = exports;
